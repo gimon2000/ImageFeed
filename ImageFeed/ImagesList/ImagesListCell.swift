9d3c0b26a299342911496412f62a8389c2f0f351
Anton <gimon2000@yandex.ru>
2024-03-11 23:02:37 +0300

    Добавил в ячейку таблицы дату, лайк, картинку Но на симуляторе ячейка не имеет скруглений и картинка не заполняет ячейку

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by gimon on 10.03.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
