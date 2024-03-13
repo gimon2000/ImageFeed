047fe23a2f04ebeb4e35bd368ef5bb360ac0ba4b
Anton <gimon2000@yandex.ru>
2024-03-13 20:04:35 +0300

    Добавил аутлеты Исправил заполнение ячейки картинкой

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -9,4 +9,8 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet private weak var imageCell: UIImageView!
+    @IBOutlet private weak var dataLabel: UILabel!
+    @IBOutlet private weak var likeButton: UIButton!
 }
