e0f6330a1b07c227c6fe1acfa856af6d0fb5d6f6
Anton <gimon2000@yandex.ru>
2024-03-16 10:13:20 +0300

    Sprint 9 Добавлен экран профиля Добавлен таббар

---
+++ ImageFeed/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ImageFeed
+//
+//  Created by gimon on 15.03.2024.
+//
+
+import Foundation
---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by gimon on 15.03.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var AvatarImage: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var nickLabel: UILabel!
+    @IBOutlet private var messageLabel: UILabel!
+    
+    @IBAction private func didClickExitButton(_ sender: Any) {
+    }
+}
