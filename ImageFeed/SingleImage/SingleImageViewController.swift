66a9ceef29b8080334c601abecc671217ae5b9d9
Anton <gimon2000@yandex.ru>
2024-03-17 21:59:44 +0300

    Реализовал открытие экрана изображения через navigation-controller-segue

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by gimon on 17.03.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    private var image: UIImage?
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    func setImage (imageView: UIImage) {
+        image = imageView
+    }
+}
