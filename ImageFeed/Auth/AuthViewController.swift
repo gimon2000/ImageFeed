6d33ae849416221379a87dd6cf1f4a3bde506bf7
Anton <gimon2000@yandex.ru>
2024-04-02 22:20:06 +0300

    sprint 10 - добавил первый экран авторизации

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by gimon on 02.04.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by gimon on 30.03.2024.
+//
+
+import Foundation
